b44ef56fad7b885b5014cafcc0ce7607b072bfd5
BuildByTushar <tusharchoudharyxpace@gmail.com>
2026-04-19 00:33:29 +0530

    updated

--- B/18_test_2218B.cpp
+++ B/18_test_2218B.cpp
@@ -27,3 +27,34 @@ int main()
     
     return 0;
 }
+
+/*
+// second attempt
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    signed int t, max, sum;
+    signed int arr[7];
+    cin >> t;
+    while (t--)
+    {
+        max = INT_MIN;
+        sum = 0;
+        for (int i = 0; i < 7; i++)
+        {
+            cin >> arr[i];;
+            if (max < arr[i])
+            {
+                max = arr[i];
+            }
+            sum -= arr[i];
+        }
+        sum += 2 * max;
+        cout << sum << endl;
+    }
+    return 0;
+}
+*/
