36c7d46fc3482e5a71c7ac98473b4970e5a85867
BuildByTushar <tusharchoudharyxpace@gmail.com>
2026-04-21 20:06:00 +0530

    Refactor and enhance logic in 54_test_148A

--- A/54_test_148A.cpp
+++ A/54_test_148A.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
-    long k, l, m, n;
-    long d;
-    cin >> k >> l >> m >> n >> d;
-    
+    long d, answer;
+    bool flag = true;
+    vector<long> arr(4);
+    for (int i = 0; i < 4; i++)
+    {
+        cin >> arr[i];
+        if (arr[i] == 1)
+        {
+            flag = false;
+        }
+        
+    }
+    cin >> d;
+    answer = d;
+    if(!flag) 
+    {
+        cout << d << endl;
+        return 0;
+    }   
+    vector<int> result(d,0);
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 1; j <= d/arr.at(i); j++)
+        {
+            result[j*arr.at(i) - 1]++;
+        }
+    }
+    for (int i = 0; i < d; i++)
+    {
+        if (result.at(i) == 0)
+        {
+            answer--;
+        }
+    }
+    cout << answer << endl;   
     return 0;
 }
