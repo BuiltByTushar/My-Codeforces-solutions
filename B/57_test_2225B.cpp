344dc1b24e7e480790f12a423f893a099dcae3ec
BuildByTushar <tusharchoudharyxpace@gmail.com>
2026-04-21 22:13:54 +0530

    to do

---
+++ B/57_test_2225B.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void reverse(string &s, int a, int b)
+{
+    while (a < b)
+    {
+        swap(s[a], s[b]);
+        a++;
+        b--;
+    }
+}
+
+void invert(string &s, int a, int b)
+{
+    for (int i = a; i <= b; i++)
+    {
+        if (s[i] == 'a')
+        {
+            s[i] = 'b';
+        }
+        else if (s[i] == 'b')
+        {
+            s[i] = 'a';
+        }
+    }
+}
+
+bool check(string &s, int n)
+{
+    while (n--)
+    {
+        if (s[n] == s[n - 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t, count, n, temp;
+    cin >> t;
+    string s, s_new;
+    bool flag;
+    while (t--)
+    {
+        vector<int> index;
+        flag = false;
+        cin >> s;
+        s_new = s;
+        count = 0;
+        n = s.size() - 1;
+        for (int i = 0; i < n; i++)
+        {
+            if (s.at(i) == s.at(i + 1))
+            {
+                count++;
+                index.push_back(i);
+            }
+        }
+        temp = 0;
+        for (int i = 0; i < count; i++)
+        {
+            if (check(s_new, n))
+            {
+                cout << "YES\n";
+                flag = true;
+                break;
+            }
+            reverse(s_new, temp, index.at(i));
+            if (check(s_new, n))
+            {
+                cout << "YES\n";
+                flag = true;
+                break;
+            }
+            else
+            {
+                invert(s_new, temp, index.at(i));
+            }
+            if (check(s_new, n))
+            {
+                cout << "YES\n";
+                flag = true;
+                break;
+            }
+            else
+            {
+                temp = index.at(i) + 1;
+                s_new = s;
+            }
+        }
+        if (!flag)
+        {
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}
